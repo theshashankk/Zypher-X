8c78640b8b456296f51f1b9fb284c52b035d3d48
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-07-12 19:48:36 +0530

    Create alive.py

---
+++ plugins/alive.py
@@ -0,0 +1,23 @@
+from telethon import events, buttons
+
+from System import ALIVE_NAME
+from System.utils import admin_cmd, sudo_cmd
+from System.zypherConfig import Config, Var
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "ᴢʏᴘʜᴇʀ ᴜsᴇʀ"
+
+@borg.on(lightning_cmd(pattern=r"alive"))
+@borg.on(sudo_cmd(pattern=r"alive", allow_sudo=True))
+async def omkvai(event):
+  omk_caption = f"**{CUSTOM_ALIVE}**\n\n"
+  omk_caption += f"**Mʏ sʏsᴛᴇᴍ ɪs ᴘᴇʀғᴇᴄᴛʟʏ ʀᴜɴɴɪɢ**\n\n"
+  omk_caption += "Aʙᴏᴜᴛ ᴍʏ sʏsᴛᴇᴍ ✗\n\n"
+  omk_caption += f"**Zʏᴘʜᴇʀ ᴜsᴇʀ** ☞ [{DEFAULTUSER}](tg://user?id={fuk})\n"
+  omk_caption += f"**Zʏᴘʜᴇʀ ᴠᴇʀ**: `{lionver}`\n"
+  omk_caption += f"**Tᴇʟᴇᴛʜᴏɴ ᴠᴇʀ** ☞ {version.__version__}\n"
+  omk_caption += f"**Zʏᴘʜᴇʀ sᴜᴘᴘᴏʀᴛ** ☞ [ᴊᴏɪɴ](https://t.me/ZypherSupport)\n"
+  omk_caption += f"**LiCense**  ☞ [𝚃𝙴𝙰𝙼 𝚉𝚈𝙿𝙷𝙴𝚁](https://github.com/Mdnoor786/Zypher-X)\n"
+  omk_caption += (
+        f"**©️ CopyRight By** ☞ [𝚉𝚈𝙿𝙷𝙴𝚁 𝚇](https://github.com/Mdnoor786/Zypher-X)\n\n"
+    )
+  await tgbot.send_file(event.chat_id, captions=omk_caption) 
